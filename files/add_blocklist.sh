3433760028996b6a6ebc9aa76899e0b1070fb16b
Stephen Walloch <saildata03@gmail.com>
2018-10-07 17:53:40 -0500

    add iptables/remove hosts

---
+++ files/add_blocklist.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Block every IP or hostname in blocklist.txt
+
+for i in $(cat /etc/blocklist.txt); do
+	echo "Blocking all traffic to and from $i"
+	/sbin/iptables -I INPUT -s $i -j DROP
+	/sbin/iptables -I OUTPUT -d $i -j REJECT
+done
