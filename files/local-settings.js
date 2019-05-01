16d401acafa83acc64bfb2f7cf946c10f05b9a1f
Stephen Walloch <saildata03@gmail.com>
2019-05-01 12:00:45 -0500

    add ff local-settings.js

---
+++ files/local-settings.js
@@ -0,0 +1,5 @@
+user_pref("browser.preferences.defaultPerformanceSettings.enabled", false);
+user_pref("general.autoScroll", false);
+user_pref("general.smoothScroll", false);
+user_pref("general.smoothScroll.pages", false);
+user_pref("browser.tabs.warnOnClose", false);
